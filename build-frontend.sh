c3f97509172f6bc12fc15abab5163e502f0e03c1
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2017-03-10 17:33:35 +0000

    Initial B.safe build

--- build-frontend.sh
+++ build-frontend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ./frontend
+cd ./bsafe
 
 npm install
 
@@ -8,12 +8,12 @@ ng build -prod
 
 cd ..
 
-cp ./frontend/dist/inline.*.bundle.js ./hat/app/org/hatdex/hat/phata/assets/js/inline.bundle.js
+cp ./bsafe/dist/inline.*.bundle.js ./hat/app/org/hatdex/hat/phata/assets/js/inline.bundle.js
 
-cp ./frontend/dist/polyfills.*.bundle.js ./hat/app/org/hatdex/hat/phata/assets/js/polyfills.bundle.js
+cp ./bsafe/dist/polyfills.*.bundle.js ./hat/app/org/hatdex/hat/phata/assets/js/polyfills.bundle.js
 
-cp ./frontend/dist/main.*.bundle.js ./hat/app/org/hatdex/hat/phata/assets/js/main.bundle.js
+cp ./bsafe/dist/main.*.bundle.js ./hat/app/org/hatdex/hat/phata/assets/js/main.bundle.js
 
-cp ./frontend/dist/vendor.*.bundle.js ./hat/app/org/hatdex/hat/phata/assets/js/vendor.bundle.js
+cp ./bsafe/dist/vendor.*.bundle.js ./hat/app/org/hatdex/hat/phata/assets/js/vendor.bundle.js
 
-cp ./frontend/dist/styles.*.bundle.css ./hat/app/org/hatdex/hat/phata/assets/stylesheets/styles.bundle.css
+cp ./bsafe/dist/styles.*.bundle.css ./hat/app/org/hatdex/hat/phata/assets/stylesheets/styles.bundle.css
