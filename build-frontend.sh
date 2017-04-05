0213b700fd32d67e01d5b661e603d36a66739067
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2017-04-05 08:54:47 +0100

    Updates B.safe user interface

--- build-frontend.sh
+++ build-frontend.sh
@@ -17,3 +17,5 @@ cp ./bsafe/dist/main.*.bundle.js ./hat/app/org/hatdex/hat/phata/assets/js/main.b
 cp ./bsafe/dist/vendor.*.bundle.js ./hat/app/org/hatdex/hat/phata/assets/js/vendor.bundle.js
 
 cp ./bsafe/dist/styles.*.bundle.css ./hat/app/org/hatdex/hat/phata/assets/stylesheets/styles.bundle.css
+
+gzip --keep --force ./hat/app/org/hatdex/hat/phata/assets/js/*.bundle.js
